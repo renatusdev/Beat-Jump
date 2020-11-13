49a5411537fe1e987cfe606153bbd35823f4234c
EthenDev <72197819+EthenDevStuff@users.noreply.github.com>
2020-11-13 23:17:33 +0200

    Text Stuffs :D

---
+++ Assets/Scripts/TextBoxStuff.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TextBoxStuff : MonoBehaviour
+{
+    public string wantedText;
+    public TMP_Text wantedTextObject;
+    public bool isTextOnScreen = false;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Y) && !isTextOnScreen)
+        {
+            StartCoroutine(MakeTextBeCool(0.01f));
+            isTextOnScreen = true;
+
+        }
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            wantedTextObject.text = "";
+            isTextOnScreen = false;
+        }
+    }
+
+    IEnumerator MakeTextBeCool(float time)
+    {
+        for (int i = 0; i < wantedText.Length; i++)
+        { 
+            wantedTextObject.text += wantedText[i];
+            yield return new WaitForSeconds(time);
+        }
+    }
+}
