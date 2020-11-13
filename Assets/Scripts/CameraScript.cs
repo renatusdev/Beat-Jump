94d18464640822fce7e99746bca3b2ca03f2c4f9
EthenDev <72197819+EthenDevStuff@users.noreply.github.com>
2020-11-13 22:02:32 +0200

    no

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    public float mouseSensitivity;
+    public Transform player;
+    public bool Paused = false;
+    private float x = 0;
+    private float y = 0;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Update()
+    {
+        x += -Input.GetAxis("Mouse Y") * mouseSensitivity;
+        y += Input.GetAxis("Mouse X") * mouseSensitivity;
+
+        x = Mathf.Clamp(x, -90, 90);
+
+        transform.localRotation = Quaternion.Euler(x, y, 0);
+        player.localRotation = Quaternion.Euler(0, y, 0);
+        transform.position = player.transform.position;
+
+
+        if (Input.GetKeyDown(KeyCode.Escape) && !Paused)
+        {
+            Cursor.lockState = CursorLockMode.None;
+            Paused = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.Escape) && Paused)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Paused = false;
+        }
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float moveSpeed;
+    public float jumpForce;
+    public LayerMask ground;
+    private float x;
+    private float z;
+    private Rigidbody rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        x = Input.GetAxisRaw("Horizontal");
+        z = Input.GetAxisRaw("Vertical");
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded())
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.VelocityChange);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.AddForce(transform.right * x * moveSpeed, ForceMode.VelocityChange);
+        rb.AddForce(transform.forward * z * moveSpeed, ForceMode.VelocityChange);
+
+        counterForce(0, 0.75f);
+    }
+
+    void counterForce(float maxSpeed, float stopStrength)
+    {
+        Vector3 vel = rb.velocity;
+        vel.y = 0;
+        float mag = vel.magnitude;
+        if (mag > maxSpeed)
+        {
+            rb.AddForce(-vel.normalized * (mag - maxSpeed) * stopStrength, ForceMode.VelocityChange);
+        }
+    }
+
+    bool isGrounded()
+    {
+        if(Physics.Raycast(transform.position, Vector3.down, 1, ground))
+        {
+            return true;
+        }
+        return false;
+    }
+}
