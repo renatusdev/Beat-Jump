7d92623bddd0b8fdc959c2a59b09f4997eba0a16
EthenDev <72197819+EthenDevStuff@users.noreply.github.com>
2020-11-13 23:20:43 +0200

    Made a spawn point object

---
+++ Assets/Scripts/SpawnPlayer.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnPlayer : MonoBehaviour
+{
+    public Transform player;
+
+    private void Start()
+    {
+        player.position = transform.position;
+    }
+}
